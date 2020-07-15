3aa50d44705c294ace433bcaeb42883b0d6f4230
Amal Khan <amalfkhan@gmail.com>
2020-07-15 09:20:43 -0700

    first commit

---
+++ src/Joke.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Joke extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Joke 1</h1>
+      </div>
+    )
+  }
+}
+
+export default Joke;
